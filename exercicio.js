212fce330bf54cad4539c8149daf6c66799b4d67
Luiza Trocino <luizatrocino@hotmail.com>
2022-03-26 09:05:35 -0300

    ajuste de código

--- exercicio.js
+++ exercicio.js
@@ -3,7 +3,7 @@ const fs = require('fs/promises')
 function soma(num1, num2){
     const p = new Promise ((resolve,reject)=>{
         if(typeof(num1)!="number"){
-            return reject()
+            return reject("Não são números")
         }
 
         fs.appendFile("resultado.txt",`${num1}+${num2}=${num1+num2}\n`).then( () => {
@@ -13,8 +13,8 @@ function soma(num1, num2){
 
     p
     .then((resultado)=> console.log("Resultado gerado!"))
-    .catch((erro) => console.log("Não são números"))
+    .catch((erro) => console.log(erro))
     
 }
 
-soma(3,20)
+soma("oi",20)
